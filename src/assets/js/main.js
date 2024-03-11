fb8597961dc91ca4261f43732f51eccce32d5576
Bakhriev <muradbakhriev@gmail.com>
2024-03-11 19:37:31 +0500

    init

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,42 @@
-import { rippleEffect } from './functions/rippleEffect';
+const burgerMenu = () => {
+	const burger = document.querySelector('.burger');
+	const mobileMenu = document.querySelector('.header__nav');
+	const overlay = document.querySelector('.overlay');
+	const body = document.body;
+	console.log(burger, mobileMenu, overlay, body);
+
+	const toggleActiveClass = () => {
+		burger.classList.toggle('active');
+		overlay.classList.toggle('active');
+		mobileMenu.classList.toggle('active');
+		body.classList.toggle('disable-scroll');
+	};
+
+	burger.addEventListener('click', toggleActiveClass);
+
+	overlay.addEventListener('click', toggleActiveClass);
+
+	window.addEventListener('resize', () => {
+		const { innerWidth } = window;
+		if (innerWidth > 991.98) {
+			burger.classList.remove('active');
+			overlay.classList.remove('active');
+			mobileMenu.classList.remove('active');
+			body.classList.remove('disable-scroll');
+		}
+	});
+};
+
+burgerMenu();
+
+const navLinks = document.querySelectorAll('.menu__link');
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		removeCurrentLinks();
+		link.classList.add('menu__link_current');
+	});
+});
+
+const removeCurrentLinks = () => {
+	navLinks.forEach(link => link.classList.remove('menu__link_current'));
+};
